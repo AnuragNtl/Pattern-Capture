004927bc977a6156031313313cc121ec6a277a78
Anurag <anuragshrm70@gmail.com>
2020-11-18 23:57:17 +0530

    HttpRelay

--- common/HttpRelay/HttpRelay.cpp
+++ common/HttpRelay/HttpRelay.cpp
@@ -4,20 +4,37 @@
 #include <boost/asio/connect.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <string>
+#include <regex>
+
+#define DOMAIN_REGEX "(?<http.+//).+(?=/)"
+#define PATH_REGEX "(?<http.+//.+?/).+$"
 
 using namespace std;
 namespace beast = boost::beast;
 using namespace boost::asio;
-namespace http = beast::http;
+namespace http = boost::beast::http;
 using tcp = boost::asio::ip::tcp;
 
 #include "HttpRelay.h"
 using namespace PatternCapture;
 
-HttpRelayDependency :: HttpRelayDependency(string url, string method, string payload, HookType hookType) : url(url), method(method), payload(payload), hookType(hookType) { }
+HttpRelayDependency :: HttpRelayDependency(string url, string method, string payload, map<string, string> headers, HookType hookType, http::verb verb) : url(url), method(method), payload(payload), headers(headers), hookType(hookType), verb(verb) {
+    this->host = extractRegex(DOMAIN_REGEX, url);
+    path = extractRegex(PATH_REGEX, url);
+}
+string HttpRelayDependency :: extractRegex(string regex, string s) {
+    smatch match;
+    regex_match(s, match, std::regex(DOMAIN_REGEX), regex_constants::match_default);
+    if(match.size() > 0) {
+        return match[0];
+    } else  {
+        return string();
+    }
+}
 
-
-string HttpRelayDependency :: getOutput(string input, map<string, string> inputParams) { }
+string HttpRelayDependency :: getOutput(string input, map<string, string> inputParams) { 
+    return string();
+}
 
 
 void HttpRelayDependency :: executeHook(const Node &node) {
@@ -31,6 +48,35 @@ HookType HttpRelayDependency :: getHookType() {
 string HttpRelayDependency :: getResponse() const {
 
     boost::asio::io_context ioc;
+    tcp::resolver resolver(ioc);
+    beast::tcp_stream stream(ioc);
+
+    auto lookup = resolver.resolve(host);
+    stream.connect(lookup);
+    http::request<http::string_body> request(verb, path, 11);
+
+    for(auto &item : headers) {
+        request.set(item.first, item.second);
+    }
+    request.body() = payload;
+    http::write(stream, request);
+
+    beast::flat_buffer buffer;
+
+    http::response<http::dynamic_body> res;
+
+    http::read(stream, buffer, res);
+
+    string responseString = beast::buffers_to_string(res.body().data());
+
+    beast::error_code errorCode;
+
+    stream.socket().shutdown(tcp::socket::shutdown_both, errorCode);
+
+    if(errorCode && errorCode != beast::errc::not_connected)
+        throw beast::system_error{errorCode};
+
+    return responseString;
 
 }
 
--- common/HttpRelay/HttpRelay.h
+++ common/HttpRelay/HttpRelay.h
@@ -4,18 +4,24 @@
 
 #include "../Common.h"
 #include "../../core/engine/hooks/Hooks.h"
+#include <map>
+#include <boost/beast/http.hpp>
 
 namespace PatternCapture {
 
     class HttpRelayDependency : public CommonDependency<string, string>, public Hook  {
 
         private:
-            string url, method, payload;
+            string host, path, url, method, payload;
+            map<string, string> headers;
             HookType hookType;
+            boost::beast::http::verb verb;
+
         protected:
             string getResponse() const;
+            string extractRegex(string, string);
         public:
-            HttpRelayDependency(string url, string method, string payload, HookType hookType);
+            HttpRelayDependency(string url, string method, string payload, map<string, string> headers, HookType hookType, boost::beast::http::verb);
             string getOutput(string input, map<string, string>);
             void executeHook(const Node &);
             HookType getHookType();
